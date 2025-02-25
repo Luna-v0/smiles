be6a5f9e06de4acdb2c2176156702dc44c7e92fd
Eduardo Dantas Luna <michaelthefear@gmail.com>
2025-02-25 19:36:47 -0300

    propagate results of rules

--- src/chem.py
+++ src/chem.py
@@ -1,5 +1,8 @@
 from typing import Optional
 
+def lookupElectricConfig(symbol:str) -> str:
+    pass
+
 
 def validate_valency_mol(isotope: Optional[int], symbol: str, chiral: Optional[int],
                          hcount: Optional[int], charge: Optional[int],
--- src/yacc.py
+++ src/yacc.py
@@ -1,8 +1,9 @@
 from sly import Parser
-from lex import SmilesLex
-import chem
+from src.lex import SmilesLex
+import src.chem as chem
 import re
 from itertools import combinations
+import json  # temporary
 
 # https://depth-first.com/articles/2020/04/20/smiles-formal-grammar/
 
@@ -16,7 +17,6 @@ def generate_combinations(rule: str):
 
     all_combinations = []
 
-    # Generate all possible ordered combinations of optional elements
     for i in range(len(optional) + 1):
         for combo in combinations(optional, i):
             ordered_combo = [p.rstrip('?') for p in parts if p.rstrip(
@@ -24,6 +24,18 @@ def generate_combinations(rule: str):
             all_combinations.append(" ".join(ordered_combo))
 
     return all_combinations
+
+
+def getAttributes(rules, properties):
+    if type(properties) != list:
+        return getattr(rules, properties, None)
+
+    values = []
+
+    for prop in properties:
+        values.append(getAttributes(rules, prop))
+
+    return values
 
 
 class SmilesParser(Parser):
@@ -55,19 +67,29 @@ class SmilesParser(Parser):
 
     @_('"[" internal_bracket "]"')  # type: ignore
     def bracket_atom(self, rules):
-        self.update_last_rule('bracket_atom')
-        pass
+        return rules.internal_bracket
+
 
     @_(*generate_combinations('isotope? hcount? symbol chiral? charge? map?')) # type: ignore
     def internal_bracket(self, rules):
-        chem.validate_valency_mol(rules.isotope, rules.symbol,
-                                  rules.chiral, rules.hcount,
-                                  rules.charge, rules.map)
+        
+        mol = getAttributes(rules, ['isotope', 'hcount', 'symbol', 'chiral', 'charge', 'map'])
+        
+        isotope, hcount, symbol, chiral, charge, mol_map = mol
+
+        print(f"Isotope: {isotope}")
+        print(f"Hcount: {hcount}")
+        print(f"Symbol: {symbol}")
+        print(f"Chiral: {chiral}")
+        print(f"Charge: {charge}")
+        print(f"Map: {mol_map}")
+
+        chem.validate_valency_mol(isotope, symbol, chiral, hcount, charge, mol_map)
         pass
 
     @_('dot_proxy', 'bond atom', 'bond rnum', 'atom', 'rnum')  # type: ignore
     def chain(self, rules):
-        self.update_last_rule('chain')
+
         pass
 
     @_('"." atom')  # type: ignore
@@ -76,72 +98,83 @@ class SmilesParser(Parser):
 
     @_('semi_symbol', 'organic_symbol')  # type: ignore
     def symbol(self, rules):
-        self.update_last_rule('symbol')
-        return rules[0] if len(rules) > 0 else None
+        return rules[0]
 
     @_('"(" inner_branch ")"')  # type: ignore
     def branch(self, rules):
         self.update_last_rule('branch')
         pass
 
-    @_('bond_dot line', 'line', 'inner_branch bond_dot line', 'inner_branch line') # type: ignore
+
+    @_('bond_dot line', 'line', 'bond_dot line inner_branch', 'line inner_branch')    # type: ignore
     def inner_branch(self, rules):
         self.update_last_rule('inner_branch')
         pass
 
     @_('bond', '"."')  # type: ignore
     def bond_dot(self, rules):
-        self.update_last_rule('bond_dot')
-        return rules[0] if len(rules) > 0 else None
+        return rules[0]
 
     @_('semi_bond_rule', '"-"')  # type: ignore
     def bond(self, rules):
-        self.update_last_rule('bond')
-        return rules[0] if len(rules) > 0 else None
+        return rules[0]
 
     @_('semi_bond')  # type: ignore
     def semi_bond_rule(self, rules):
-        self.update_last_rule('semi_bond_rule')
-        pass
+        return rules[0]
 
     @_('"H"', 'semi_organic_rule')  # type: ignore
     def organic_symbol(self, rules):
-        self.update_last_rule('organic_symbol')
-        pass
+        return rules[0]
 
     @_('semi_organic_symbol')  # type: ignore
     def semi_organic_rule(self, rules):
-        pass
+        return rules[0]
 
     @_('organic_symbol', 'bracket_atom')  # type: ignore
     def atom(self, rules):
-        self.update_last_rule('atom')
-        pass
+        return rules[0]
 
     @_('digit', '"%" digit digit ')  # type: ignore
     def rnum(self, rules):
-        self.update_last_rule('rnum')
-        pass
+        if rules[0] == '%':
+            return int(''.join(rules[1:]))
+
+        return int(rules[0])
 
     @_('digit digit digit', 'digit digit', 'digit')  # type: ignore
     def isotope(self, rules):
-        self.update_last_rule('isotope')
-        pass
+        return int(''.join(rules))
 
     @_('"H" digit', '"H"')  # type: ignore
     def hcount(self, rules):
-        self.update_last_rule('hcount')
-        pass
+        if len(rules) == 1:
+            return 1
+
+        return int(rules.digit)
 
     @_('"+"', '"+" "+"', '"-"', '"-" "-"', '"-" fifteen', '"+" fifteen')  # type: ignore
     def charge(self, rules):
-        self.update_last_rule('charge')
-        pass
+        if len(rules) == 1:
+            return 1 if rules[0] == "+" else -1
+
+        if rules[1] == '-':
+            return -2
+
+        if rules[1] == '+':
+            return 2
+
+        if rules[0] == '-':
+            return rules.fifteen * -1
+
+        return rules.fifteen
 
     @_('":" digit digit digit', '":" digit digit', '":" digit')  # type: ignore
     def map(self, rules):
-        self.update_last_rule('map')
-        pass
+
+        digits = rules[1:]
+
+        return int(''.join(digits))
 
     @_('"@"', '"@" "@"')  # type: ignore
     def chiral(self, rules):
@@ -150,8 +183,16 @@ class SmilesParser(Parser):
 
     @_('digit digit', 'digit')  # type: ignore
     def fifteen(self, rules):
-        self.update_last_rule('fifteen')
-        pass
+
+        if len(rules) == 2:
+            x = int(rules[0]+rules[1])
+
+            if x > 15:
+                raise "cannot execeed 15"
+
+            return x
+
+        return int(rules.digit)
 
 
 parser = SmilesParser()
@@ -163,8 +204,7 @@ def validateSMILES(mol: str) -> bool:
         parser.parse(lexer.tokenize(mol))
         return True
     except Exception as e:
-        print(e)
         return False
 
 
-print(validateSMILES("NCCNCCNCCN"))
+print(validateSMILES("[Cl+]"))
