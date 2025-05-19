4db4e00dc4bc694a26284c92d2eb88e476717193
luna-v0 <edudaluna2000@gmail.com>
2025-05-19 15:15:12 -0300

    fixing ring tests

--- tests/validator/test_parser_values.py
+++ tests/validator/test_parser_values.py
@@ -1,9 +1,9 @@
 from validator.yacc import validate_smiles
 
 def test_benzene():
-    assert validate_smiles("c1ccccc1")[0] == True
-    assert validate_smiles("C1=CC=CC=C1")[0] == True
-    assert validate_smiles("C1=CC=C=C1C")[0] == True
-    assert validate_smiles("[H]c1c([H])c([H])c([H])c([H])c1[H]")[0] == True
+    assert validate_smiles("c1ccccc1") == (True, None)
+    assert validate_smiles("C1=CC=CC=C1") == (True, None)
+    assert validate_smiles("C1=CC=C=C1C") == (True, None)
+    assert validate_smiles("[H]c1c([H])c([H])c([H])c([H])c1[H]") == (True, None)
     assert validate_smiles("c1ccccc1C")[0] == False
-    assert validate_smiles("c1[c]cccc1")[0] == True
+    assert validate_smiles("c1[c]cccc1") == (True, None)
