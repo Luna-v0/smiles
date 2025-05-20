d858e54292335ddfc577b72d13f0a9fd5ea15b68
Eduardo Dantas Luna <edudaluna2000@gmail.com>
2025-05-19 22:06:40 -0300

    Forgot to commit this in the last commit

--- tests/chem/test_electrons.py
+++ tests/chem/test_electrons.py
@@ -1,5 +1,12 @@
 from chem import chem, Atom, BracketAtom
 
+def test_atom():
+    assert Atom("C") == Atom("C")
+    assert Atom("C") != Atom("N")
+
+    assert Atom("C") != BracketAtom("C", 1)
+
+
 def test_valency():
     pass
 
