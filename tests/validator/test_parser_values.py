8703318635dc5a9a9c52cbfbacf6ca25b7e67448
Eduardo Dantas Luna <edudaluna2000@gmail.com>
2025-05-21 16:20:50 -0300

    Made tests more informative

--- tests/validator/test_parser_values.py
+++ tests/validator/test_parser_values.py
@@ -2,9 +2,26 @@ from validator.yacc import validate_smiles
 
 
 def test_benzene():
-    assert validate_smiles("c1ccccc1") == (True, None)
-    assert validate_smiles("C1=CC=CC=C1") == (True, None)
-    assert validate_smiles("C1=CC=C=C1C") == (True, None)
-    assert validate_smiles("[H]c1c([H])c([H])c([H])c([H])c1[H]") == (True, None)
-    assert validate_smiles("c1ccccc1C")[0] == False
-    assert validate_smiles("c1[c]cccc1") == (True, None)
+    assert validate_smiles("c1ccccc1") == (
+        True,
+        None,
+    ), "Failed to validate aromatic ring with lowercase syntax"
+    assert validate_smiles("C1=CC=CC=C1") == (
+        True,
+        None,
+    ), "Failed to validate aromatic ring with uppercase syntax"
+    assert validate_smiles("C1=CC=C=C1C") == (
+        True,
+        None,
+    ), "Failed to validate aromatic ring with uppercase syntax with mapping at the end"
+    assert validate_smiles("[H]c1c([H])c([H])c([H])c([H])c1[H]") == (
+        True,
+        None,
+    ), "Failed to validate aromatic ring with bracket hydrogens"
+    assert (
+        validate_smiles("c1ccccc1C")[0] == False
+    ), "Cannot join aromatic carbon ring with a Non-aromatic carbon"
+    assert validate_smiles("c1[c]cccc1") == (
+        True,
+        None,
+    ), "Failed to validate aromatic ring with lowercase syntax and bracketed carbon"
