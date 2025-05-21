fa749bd88e55d58ea380ebfd32a370a5ef18f790
Eduardo Dantas Luna <edudaluna2000@gmail.com>
2025-05-20 21:11:21 -0300

    Added more tests

--- tests/chem/test_electrons.py
+++ tests/chem/test_electrons.py
@@ -3,9 +3,14 @@ from chem import chem, Atom, BracketAtom
 def test_atom():
     assert Atom("C") == Atom("C")
     assert Atom("C") != Atom("N")
-
-    assert Atom("C") != BracketAtom("C", 1)
-
+    assert Atom("C") != BracketAtom("C", 0)
+    
+def test_bracket_atom():
+    assert BracketAtom("He",hidrogens=1) == BracketAtom("He", hidrogens=1)
+    assert BracketAtom("He",hidrogens=1) != BracketAtom("He", hidrogens=2)
+    
+    assert BracketAtom("He", hidrogens=1).compute_valency() == False
+    assert BracketAtom("Li", charge=1).compute_valency() == True
 
 def test_valency():
     pass
